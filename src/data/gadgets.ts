ebfd8f32f1b62e5a684208f8e6bef1c5eb167beb
AniMaster Anon <898004546@qq.com>
2026-05-28 13:20:01 +0800

    - 更新了todoflow的安装包

--- src/data/gadgets.ts
+++ src/data/gadgets.ts
@@ -29,8 +29,8 @@ export const gadgets: Gadget[] = [
     },
     icon: '/gadgets/todoflow.png',
     platform: 'Windows',
-    version: 'v0.1.5',
+    version: 'v0.2.0',
     size: '≈ 1.5 MB',
-    downloadHref: '/gadgets/TodoFlow_0.1.5_x64-setup.exe',
+    downloadHref: '/gadgets/TodoFlow_0.2.0_x64-setup.exe',
   },
 ]
